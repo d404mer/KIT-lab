b1d21f9a17cb4de40cf634b484fbc3946b38739a
d0h <dev1lc3st@gmail.com>
2024-09-06 10:21:55 +0300

    ПР1 ПиТПМ начало

---
+++ 3 курс/ПиТПМ/практика/ПР1/ПР1/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Calculator
+{
+    public partial class MainWindow : Window
+    {
+        private string currentInput = string.Empty;
+        private string operatorSelected = string.Empty;
+        private double result = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                currentInput += button.Content.ToString();
+                ResultTextBox.Text = currentInput;
+            }
+        }
+
+        private void OperatorButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                if (currentInput != string.Empty)
+                {
+                    if (operatorSelected != string.Empty)
+                    {
+                        CalculateResult();
+                    }
+                    result = double.Parse(currentInput);
+                    operatorSelected = button.Content.ToString();
+                    currentInput = string.Empty;
+                }
+            }
+        }
+
+        private void EqualButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentInput != string.Empty && operatorSelected != string.Empty)
+            {
+                CalculateResult();
+                operatorSelected = string.Empty;
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            currentInput = string.Empty;
+            operatorSelected = string.Empty;
+            result = 0;
+            ResultTextBox.Text = string.Empty;
+        }
+
+        private void CalculateResult()
+        {
+            double newValue = double.Parse(currentInput);
+
+            switch (operatorSelected)
+            {
+                case "+":
+                    result += newValue;
+                    break;
+                case "-":
+                    result -= newValue;
+                    break;
+                case "*":
+                    result *= newValue;
+                    break;
+                case "/":
+                    if (newValue != 0)
+                    {
+                        result /= newValue;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ошибка: Деление на ноль");
+                        ClearButton_Click(null, null); 
+                    }
+                    break;
+                case "%":
+                    result %= newValue;
+                    break;
+            }
+
+            currentInput = result.ToString();
+            ResultTextBox.Text = currentInput;
+        }
+    }
+}
+
