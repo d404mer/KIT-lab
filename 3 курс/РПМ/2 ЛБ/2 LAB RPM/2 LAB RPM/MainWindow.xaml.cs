d4a079dbb3974fc08eb9297fec7524c72b8e1aad
d0h <dev1lc3st@gmail.com>
2024-09-26 17:26:26 +0300

    2 лаба 1 задание

--- 3 курс/РПМ/2 ЛБ/2 LAB RPM/2 LAB RPM/MainWindow.xaml.cs
+++ 3 курс/РПМ/2 ЛБ/2 LAB RPM/2 LAB RPM/MainWindow.xaml.cs
@@ -1,28 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace _2_LAB_RPM
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Random random = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void EscapeButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // Получаем текущие координаты кнопки
+            double buttonX = EscapeButton.Margin.Left;
+            double buttonY = EscapeButton.Margin.Top;
+
+            // Генерируем новые координаты, смещая кнопку случайным образом
+            double newX = buttonX + random.Next(-50, 50);
+            double newY = buttonY + random.Next(-50, 50);
+
+            // Убедимся, что кнопка не уходит за границы окна
+            if (newX < 0) newX = 0;
+            if (newX + EscapeButton.Width > this.Width) newX = this.Width - EscapeButton.Width;
+            if (newY < 0) newY = 0;
+            if (newY + EscapeButton.Height > this.Height) newY = this.Height - EscapeButton.Height;
+
+            // Перемещаем кнопку
+            EscapeButton.Margin = new Thickness(newX, newY, 0, 0);
         }
     }
 }
