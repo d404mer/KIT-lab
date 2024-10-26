9e7f91f2684fc1e0cf64740aa3c7734bbc7a1813
druap <76074150+d404mer@users.noreply.github.com>
2024-10-26 19:16:56 +0300

    [РПМ ЛБ4] промежутка или финал
    посмотрим

--- 3 курс/РПМ/4 ЛБ/4 laba - 2/4 laba - 2/obj/Debug/net8.0-windows/4 laba - 2.AssemblyInfo.cs
+++ 3 курс/РПМ/4 ЛБ/4 laba - 2/4 laba - 2/obj/Debug/net8.0-windows/4 laba - 2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("4 laba - 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04ce0d3a66eaa2cb306abf3c2bad334a86b8251f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8337176290adda67b4500c0a9056be09ce1f84ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("4 laba - 2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("4 laba - 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
